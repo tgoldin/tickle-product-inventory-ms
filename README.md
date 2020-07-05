f3574db48a5669d9838020760f1909fa161f9462
Tony <tgoldin@euler.01.vpc>
2020-07-05 16:42:12 +0100

    Added liquibase initialization

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tickle Product Inventory SpringBoot Microservice
+
+Batch process product inventory files so new product inventory details 
+can be loaded into the product inventory repository. 
---
+++ src/test/java/com/tpg/tickle/pi/ProductInventoryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.tickle.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
